3a29828123424694d9efac5d5b5910fc92f46828
Sharukesh3 <prosharu3706@gmail.com>
2024-05-07 23:50:41 +0530

    Structuring the webpage

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/summarizer')
+def summarizer():
+    return render_template('summarizer.html')
+
+@app.route('/q_and_a_bot')
+def qa():
+    return render_template('qa.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
