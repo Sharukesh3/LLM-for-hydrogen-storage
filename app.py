ddf311192064ca7981afd5fe846766358af9e53f
Sharukesh3 <prosharu3706@gmail.com>
2024-05-09 16:03:48 +0530

    fixing upload doc button #1

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 UPLOAD_FOLDER = 'uploaded_pdf'  # Directory to save uploaded files
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+Uploaded_pdf_path = "C:\\familyfolders\\profolders\\Collage stuff\\Sem 2 project\\MI\\uploaded_pdf\\"
 
 @app.route('/')
 def index():
@@ -16,23 +17,25 @@ def index():
 def summarizer():
     result = None
     uploaded_pdf = None
-    
+    uploaded_files = []  # Initialize an empty list to store uploaded filenames
+
     if request.method == 'POST':
         input_text = request.form.get('input_textarea')
         pdf_file = request.files.get('upload-doc')
 
         if pdf_file:
             uploaded_pdf = pdf_file
-            pdf_filename = secure_filename(uploaded_pdf.filename)
+            pdf_filename = secure_filename(pdf_file.filename)
             pdf_file.save(os.path.join(app.config['UPLOAD_FOLDER'], pdf_filename))
+            uploaded_files.append(pdf_filename)  # Add the filename to the list
 
             # Call generate_summary with the uploaded PDF file
-            result = mld.summarize(pdf_file=uploaded_pdf)
+            result = mld.summarize(pdf_file=Uploaded_pdf_path + pdf_filename)
         elif input_text:
             # Call generate_summary with the input text
             result = mld.summarize(input_text=input_text)
 
-    return render_template('summarizer.html',result=result)
+    return render_template('summarizer.html', result=result, uploaded_files=uploaded_files)
 
 @app.route('/q_and_a_bot')
 def qa():
